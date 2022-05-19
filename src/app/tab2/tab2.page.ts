8042b1181602938105fa59b0c981c6ee9c12080f
João Érik da Silva Crisóstomo <joao.crisostomo01@etec.sp.gov.br>
2022-05-19 14:17:39 -0300

    Adicionando itens na página do carrinho

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  items = [
+    {
+      nome: 'LÁMEN PICANTE',
+      imagem: 'https://cdn.shopify.com/s/files/1/0097/2013/5731/products/beef_e3e61b80-3d35-4f42-a484-1592150bac3d_1000x.png',
+      preco: 39.99
+    },
+    {
+      nome: 'PÃO DE FORMA',
+      imagem: 'https://s.cornershopapp.com/product-images/2379642.png?versionId=veFmf.vZWXFcUOMeQHGbVtwuuunwoDL5',
+      preco: 29.99
+    },
+    {
+      nome: 'BOLINHO DE ARROZ',
+      imagem: 'https://cdn140.picsart.com/289276163022211.png',
+      preco: 14.98
+    }
+  ];
+
   constructor() {}
 
 }
